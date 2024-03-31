b97cb4278ddeb61605eaa9120c9c8989c3cfbb1e
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-03-31 01:09:04 -0700

    Update and rename OraclePLSQL to OraclePLSQL.sql

---
+++ OraclePLSQL.sql
@@ -0,0 +1,12 @@
+/*
+Oracle uses PL/SQL (Procedural Language SQL), that allows users to add conditional statements, looping statements, user defined functions and procedures, exceptions, etc to create actual programs.
+
+There are three major sections in a PL/SQL block statement
+
+1. DECLARE section is used to declare variables, constants, and user defined procedures and functions. This is optional.
+2. BEGIN section contains the written executable logical code. 
+3. EXCEPTION
+END;
+*/
+
+
