d8d2222603da94f8ef7b950bf253af377ebc1e11
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-01 15:35:08 -0700

    Update 📶2IntermediateQueries.sql

--- 📶2IntermediateQueries.sql
+++ 📶2IntermediateQueries.sql
@@ -1,1 +1,12 @@
+/*
+This is a SQL guide for intermediate level queries. 
+We will be going over subqueries, handling NULL values, CTEs, Recursion, and windows functions.
+*/
+
+
+-- SELF JOIN is when you join a table to itself. Used for hierchical data, when you need to find data connected to other data values within the same table.
+    SELECT TableName.ColumnName, TableName2.ColumnName
+    FROM TableName tbl1
+    INNER JOIN tbl1.IDColumnName = tbl2.IDColumnName
+
 
