4303a2c66f774dec0d588d4476f8988d624860c2
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-01 15:43:09 -0700

    Update 📶2_IntermediateQueries.sql

--- 📶2_IntermediateQueries.sql
+++ 📶2_IntermediateQueries.sql
@@ -1,6 +1,6 @@
 /*
 This is a SQL guide for intermediate level queries. 
-We will be going over subqueries, handling NULL values, CTEs, Recursion, and windows functions.
+We will be going over subqueries, CTEs/Recursive JOINS, handling NULL values, and windows functions.
 */
 
 
@@ -10,3 +10,11 @@ We will be going over subqueries, handling NULL values, CTEs, Recursion, and win
     INNER JOIN tbl1.IDColumnName = tbl2.IDColumnName
 
 
+-- Comomon Table Expressions are  JOIN is when you join a table to itself. Used for hierchical data, when you need to find data connected to other data values within the same table.
+    WITH cteTable
+    AS
+    (
+        SELECT TableName.ColumnName, TableName2.ColumnName
+        FROM TableName tbl1
+        INNER JOIN tbl1.IDColumnName = tbl2.IDColumnName
+    )
