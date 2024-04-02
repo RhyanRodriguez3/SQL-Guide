5fd12c9f0a10f8834980af1742a9e036e4c45382
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-01 22:26:27 -0700

    Update 📶1_BeginnerQueries.sql

--- 📶1_BeginnerQueries.sql
+++ 📶1_BeginnerQueries.sql
@@ -8,6 +8,7 @@ We will be going over statement structure (the syntax and major clauses) and bas
         SELECT ColumnNames(s) AS RenameColumn(s)
         FROM TableName
         JOIN TableName1 AS RenamedTable ON RenamedTable1.IDColumnName = RenamedTable2.IDColumnName
+        JOIN TableName3 AS RenamedTable3 ON RenamedTable2.IDColumnName = RenamedTable3.IDColumnName -- To JOIN more than one table, simply repeat the statement and connect the ID columns.
           /*
           JOIN clauses allow you to pull data from multiple tables.
           INNER JOIN only pulls rows from table columns whose ID match, and exclude values that don't.
@@ -28,18 +29,27 @@ We will be going over statement structure (the syntax and major clauses) and bas
         LIMIT;
 
 
--- SELF JOIN is when you join a table to itself. Used for hierchical data, when you need to find data connected to other data values within the same table.
+-- SELF JOIN is when you join a table to itself. Used for hierchical data, when you need to find data IDs connected to other columns within the same table and creating duplicate columns.
     SELECT TableName.ColumnName, TableName2.ColumnName
     FROM TableName tbl1
-    INNER JOIN tbl1.IDColumnName = tbl2.IDColumnName
+    INNER JOIN tbl1.IDColumnName = tbl2.IDColumnName;
 
-            
+-- UNION statement appends rows from separate tables with double SELECT statements.
+    SELECT * FROM TableName1 WHERE YourCondition, 'YourBin' AS BinName  -- This creates a column with your string value as the entire column's value.
+    UNION -- DISTINCT is default or you can use ALL to append it all
+    SELECT * FROM TableName2 WHERE YourCondition, 'YourBin' AS BinName;        
+
+
 -- Functions are calculations to handle dataset values. For a list of all SQL functions and syntax, refer to the RDBMS' documentation. The ones below are the most commonly used.
-        FUNCTIONS
-                SUM(), AVG(), MIN(), MAX(), COUNT(), STDEV() / STDEV_SAMP() / STDEV_POP(), VAR()
-                HAVING -- Used to replace the WHERE clause for aggreate functions.
-                GROUP_CONCAT()
-                DISTINCT Removes duplicate records from a column.
+        String Functions -- Deals with string/text columns values
+            LENGTH(ColumnName), UPPER(), 'L or R'TRIM(), LEFT(), RIGHT()
+            CONCAT() /*used to combine column values*/ , FORMAT() /* Used to change the format of the value*/
+            SUBSTRING(), -- This is great 
+            REPLACE() --
+        Aggregate Functions
+            SUM(), AVG(), MIN(), MAX(), COUNT(), STDEV() / STDEV_SAMP() / STDEV_POP(), VAR()
+            GROUP_CONCAT()
+            DISTINCT Removes duplicate records from a column.
 
 
 
