2d00a8ebb5e4c20857669801e015a4ee4145f0c2
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-01 17:24:52 -0700

    Update 📶2_IntermediateQueries.sql

--- 📶2_IntermediateQueries.sql
+++ 📶2_IntermediateQueries.sql
@@ -4,11 +4,24 @@ We will be going over subqueries, CTEs/Recursive JOINS, handling NULL values, an
 */
 
 
+-- Hnadling NULL or Empty string. NULL is the absence of a value. Empty is a string.
+    SELECT TableName.ColumnName
+    FROM TableName tbl1
+    WHERE ISNULL(ColumnName) = ''
+        
+-- Subqueries is creating a query and using the results from that query as a substitute for the table. TIP: Instead of multiple OR clauses, use IN clause.
+    SELECT TableName.ColumnName
+    FROM TableName tbl1
+    WHERE ColumnName IN (
+                        SELECT DISTINCT TableName.ColumnName  -- A query run inside of another query.
+                        FROM TableName tbl1
+                        WHERE ColumnName IS NOT NULL
+                        )
+
 -- SELF JOIN is when you join a table to itself. Used for hierchical data, when you need to find data connected to other data values within the same table.
     SELECT TableName.ColumnName, TableName2.ColumnName
     FROM TableName tbl1
     INNER JOIN tbl1.IDColumnName = tbl2.IDColumnName
-
 
 -- Comomon Table Expressions are  JOIN is when you join a table to itself. Used for hierchical data, when you need to find data connected to other data values within the same table.
     WITH cteTable
