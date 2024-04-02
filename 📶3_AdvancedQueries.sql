60e1b3062e389847cef4171ba90cb86d20f868cf
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-01 18:48:38 -0700

    Update 📶3_AdvancedQueries.sql

--- 📶3_AdvancedQueries.sql
+++ 📶3_AdvancedQueries.sql
@@ -3,4 +3,10 @@ This is a SQL guide for advanced level queries.
 We will be going over window functions, merge queries, and pivot queries.
 */
 
-
+-- How to handle duplicate rows.
+WITH TableCTE AS
+    (
+    SELECT*,  ROW_NUMBER() OVER(PARTITION BY ID ORDER BY ID) AS RowNumber
+    FROM Employees
+    )
+DELETE FROM TableCTE WHERE RowNumber > 1 
