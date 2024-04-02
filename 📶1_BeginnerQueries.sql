69a2933bf519fe30abaa5ee216f01c9823466069
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-01 21:24:27 -0700

    Update 📶1_BeginnerQueries.sql

--- 📶1_BeginnerQueries.sql
+++ 📶1_BeginnerQueries.sql
@@ -1,6 +1,6 @@
 /*
 This is a SQL guide for beginner level queries. 
-We will be going over statement structure (the syntax and major clauses) and aggregate functions for data manipulation.
+We will be going over statement structure (the syntax and major clauses) and basic functions for data manipulation.
 */
 
 
@@ -10,24 +10,25 @@ We will be going over statement structure (the syntax and major clauses) and agg
         JOIN TableName1 AS RenamedTable ON RenamedTable1.IDColumnName = RenamedTable2.IDColumnName
           /*
           JOIN clauses allow you to pull data from multiple tables.
-          INNER JOIN only pulls rows from tables that match, and exclude values that don't.
+          INNER JOIN only pulls rows from table columns whose ID match, and exclude values that don't.
           LEFT/RIGHT JOINs pull rows based on the original table columns. 
-          FULL OUTER JOIN pulls all table columns.
+          FULL OUTER JOIN pulls all table columns and rows.
           */
         WHERE YourCondition -- Filters rows based on specified conditions. You define the conditions with operators. 
           /*
           Operators: 
-            '=', '>=' (Greater than or equal to), '<>' Not Equal, 
+            '=', '>=' (Greater than or equal to), '<>' (Not Equal), 
             'AND' (Specify multiple conditions), 'BETWEEN' (Specify a range), 'LIKE' (Search for Patterns), 'IN' (Specify your desired values)
-          Strings/text values are
+          String/text values are:
             '%' wildcard which represents zero or multiple characters, '_' represents a single character*
           */
-        GROUP BY ColumnName(s) -- Used to group rows that have the same values into summary rows.
+        GROUP BY ColumnName(s) -- Used to group rows that have the same values into summary rows. Often followed by a function.
+        HAVING FUNCTION(ColumnName) -- The HAVING clause replaces the WHERE clause when you need to specify a function as a condition.
         ORDER BY ColumnNameYouWantSorted ON SpecifiedCriteria -- Sort the query result.
-        LIMIT / TOP;
+        LIMIT;
 
 
--- Functions are calculations to handle dataset values. For a list of all SQL functions and syntax, refer to the RDBMS documentation. The ones below are the most commonly used.
+-- Functions are calculations to handle dataset values. For a list of all SQL functions and syntax, refer to the RDBMS' documentation. The ones below are the most commonly used.
         FUNCTIONS
                 SUM(), AVG(), MIN(), MAX(), COUNT(), STDEV() / STDEV_SAMP() / STDEV_POP(), VAR()
                 HAVING -- Used to replace the WHERE clause for aggreate functions.
