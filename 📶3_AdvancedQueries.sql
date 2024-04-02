ecab1852a31084b3669f481498fa50d29513a8aa
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-02 01:06:52 -0700

    Update 📶3_AdvancedQueries.sql

--- 📶3_AdvancedQueries.sql
+++ 📶3_AdvancedQueries.sql
@@ -1,6 +1,6 @@
 /*
 This is a SQL guide for advanced level queries. 
-We will be going over merge queries and pivot queries, CTEs and temptables.
+We will be going over CTEs and temptables, recursive/hierachical queries, merge queries, and pivot queries. 
 */
 
 -- How to handle duplicate rows.
@@ -12,7 +12,7 @@ WITH TableCTE AS
 DELETE FROM TableCTE WHERE RowNumber > 1 
 
     
--- Common Table Expressions are ...
+-- Common Table Expressions are 
     WITH cteTable
     AS
     (
@@ -20,3 +20,15 @@ DELETE FROM TableCTE WHERE RowNumber > 1
         FROM TableName tbl1
         INNER JOIN tbl1.IDColumnName = tbl2.IDColumnName
     )
+
+-- Merge queries are ...
+    MERGE INTO target_table USING source_table 
+    ON merge_condition
+    WHEN MATCHED THEN 
+        UPDATE SET column1 = value1, column2 = value2,...
+    WHEN NOT MATCHED THEN 
+        INSERT (column1, column2,...) VALUES (value1, value2,...)
+    WHEN NOT MATCHED BY SOURCE THEN
+        DELETE;
+
+https://www.youtube.com/watch?v=C7CPXeEvKN0
