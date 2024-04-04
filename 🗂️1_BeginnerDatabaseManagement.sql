d6c793c9841bef2a73b1426ee70413863d3a84d6
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-03 18:16:05 -0700

    Update 🗂️1_BeginnerDatabaseManagement.sql

--- 🗂️1_BeginnerDatabaseManagement.sql
+++ 🗂️1_BeginnerDatabaseManagement.sql
@@ -1,3 +1,5 @@
 /*
 There are many RDBMS, but this is how the DB engine works.
+
+SQL query optimization involves using indexes, avoiding unnecessary columns or rows in the SELECT statement, optimizing WHERE clauses, and using appropriate JOIN types.
 */
