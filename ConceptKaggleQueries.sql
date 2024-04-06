7246aaa073201a31f0699ff8613053f67f5ab3bd
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-05 23:00:21 -0700

    Update ConceptKaggleQueries.sql

--- ConceptKaggleQueries.sql
+++ ConceptKaggleQueries.sql
@@ -1,8 +1,12 @@
--- Data sourced from RGRIFFIN Kaggle dataset "120 years of Olympic history: athletes and results"
-🪜 Cor
-Secret To Optimizing SQL Queries - Understand The SQL Execution Order - https://www.youtube.com/watch?v=BHwzDmr6d7s
-  https://www.kaggle.com/datasets/atharvaarya25/financials/data
-https://www.youtube.com/watch?v=XruOBp7yPXU&t=401s
+/*
+This is a no-bs project to get me ready using SQL to query and manipulate databases.
+Download SSMS or use web browser SQL lite: https://sqliteonline.com/syntax/all_functions
+Dataset #1: https://www.kaggle.com/datasets/atharvaarya25/financials/data
+Dataset #2: RGRIFFIN Kaggle dataset "120 years of Olympic history: athletes and results"
+*/
+
+
+
 Table 1 could possibly have duplicates Write a query to read unique records from Table 1.
 Display average GPA by college and academic year
 Display the top 3 students within each college
