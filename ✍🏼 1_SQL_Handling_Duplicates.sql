f1aa4285c41acf9ab9f5bf21d7fe1c78ea65e8ab
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-08 11:38:57 -0700

    Update ✍🏼 1_SQL_Handling_Duplicates.sql

--- ✍🏼 1_SQL_Handling_Duplicates.sql
+++ ✍🏼 1_SQL_Handling_Duplicates.sql
@@ -1,8 +1,10 @@
-/* 
-Learn how to use sql to identify nulls. 
-*/
+/* ====================================================== 
+Guide on how to handle duplicates in a data set with SQL.
 
--- There are two types of duplicates. Duplicate 1 is when row values are duplicated based on column values. Duplicate 2 is when all rows are duplicates in the table, including the ID column.
+There are two types of duplicates. 
+#1 - Duplicates where row values are the same based on COLUMNS. 
+#2 - Duplicates where ALL ROWS are the exact same in the table, including the ID column.
+========================================================= */
 
 -- Sample Data
 drop table if exists cars;
@@ -24,21 +26,20 @@ insert into cars values (6, 'Ioniq 5', 'Hyundai', 'Green', 2021); -- Rerun these
 
 SELECT * FROM cars ORDER BY model, brand;
 
-/* ==========================================================================
-   <<<<>>>> Scenario 1: Data duplicated based on SOME of the columns <<<<>>>>
-   ========================================================================== */
+/* ========================
+Duplicate Type #1 Solutions
+========================= */
 
-
---> SOLUTION 1: Delete using Unique identifier. *SSMS ERROR WIP* ERROR DESC: The ORDER BY clause is invalid in views, inline functions, derived tables, subqueries, and common table expressions, unless TOP, OFFSET or FOR XML is also specified.
-------------------------------------------------------------------------------------------------------------------------------------
+--> SOLUTION 1: Delete using Unique identifier. 
+-----------------------------------------------
 DELETE FROM cars	-- Step #2: If the dataset already has unique ID, create a SELECT statement to find the ones you want to delete.
 WHERE id IN ( 
-			SELECT model, brand, COUNT(*) AS CountOfID    -- Aggregate functions count each row and return a summary row.
-			FROM cars	
-			GROUP BY model, brand    -- Step #1: Define what a duplicate is based on column values. Then create a SELECT statement that results in the IDs of those rows.
-			HAVING COUNT(*) > 1
-			ORDER BY CountOfID
-			); 
+	     SELECT model, brand, COUNT(*) AS CountOfID    -- Aggregate function used to count each row.
+	     FROM cars	
+	     GROUP BY model, brand    -- Step #1: Define the duplicate values in the columns, then group them.
+	     HAVING COUNT(*) > 1
+	     ORDER BY CountOfID
+	    ); 
 
 
 --> SOLUTION 2: Delete using SELF JOIN, which joins the table into itself.
@@ -46,12 +47,14 @@ WHERE id IN (
 DELETE FROM cars
 WHERE id IN ( SELECT c1.id   
               FROM cars c1
-              join cars c2 on c1.model = c2.model and c1.brand = c2.brand    -- Step #1: This part joins the table into itself, and results two tables (1 original and 1 copy). This part uses columns as identifiers. TIP: When JOIN not specified, defaults to INNER JOIN.
-              WHERE c1.id > c2.id);    -- Step #2: This performs the operation on each row and returns rows that result NO.
+              JOIN cars c2    -- TIP: When JOIN not specified, defaults to INNER JOIN.
+		ON c1.model = c2.model AND c1.brand = c2.brand    -- Step #1: This part joins the table into itself using column values as IDs, then attaches the column.								     
+              WHERE c1.id > c2.id    -- Step #2: This performs the operation on each row and returns rows that result NO. NO means the grouped values return duplicates.
+	     );    
 
 
---> SOLUTION 3: Using Window function
--------------------------------------
+--> SOLUTION 3: Using the ROW_NUMBER Window function.
+-----------------------------------------------------
 DELETE FROM cars
 WHERE id IN ( SELECT id
               FROM (SELECT *
@@ -63,19 +66,16 @@ WHERE id IN ( SELECT id
 --> SOLUTION 4: Using MIN function. This deletes MULTIPLE duplicate records.
 ----------------------------------------------------------------------------
 DELETE FROM cars
-WHERE id not in ( SELECT min(id) AS MinID    -- The idea is to create an ID column that is unique. To create a result set that contains all columns, I have to manually input each column name.
+WHERE id not in ( SELECT MIN(id) AS MinID    -- Step #2: This part used the MIN function to create a column that returns the smallest ID column values.
                   FROM cars
-                  GROUP BY model, brand
-				  ORDER BY MinID ASC
-				 );
+                  GROUP BY model, brand    -- Step #1: This part filters the table based on these columns. 
+		  ORDER BY MinID ASC
+		);
 
-/*
-This is where I started to understand the intent but SSMS and MySQL are different so I just copied the syntax and commented. This section contains things I need to RESEARCH DOCUMENTATION.
-===========================================================================================================================================================================================
-*/
---> SOLUTION 5: Create a backup table, drop the original table, then rename the backup as the original. Meant for large datasets, dropping the table and recreating it is faster than using the DELETE clause. Only meant for dev environment.
---------------------------------------------------------------------------------------------------------------------------------------
-DROP TABLE if exists cars_bkp;
+
+--> SOLUTION 5: Create a backup table, drop the original table, then rename the backup as the original. 
+---------------------------------------------------------------------------------------------------------
+DROP TABLE if exists cars_bkp;    --Deleting the entire table is faster than using the DELETE statement. Only meant for dev environment.
 CREATE TABLE cars_bkp -- [CREATE TABLE IF NOT EXISTS] is not allowed in SSMS. This part creates a backup table. LOOK UP HOW TO MAKE A BACKUP TABLE USING SSMS.
 as
 SELECT * FROM cars where 1=2;    -- Step #1: This returns 0 records since 1=2 is not true. This part is supposed to be a shortcut to create the original table and keep the structure without copying the CREATE TABLE statement.
@@ -110,7 +110,6 @@ insert into cars
 select * from cars_bkp;
 
 drop table cars_bkp;
-
 
 
 
