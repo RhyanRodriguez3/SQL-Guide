17a509c9fe908ff26fb67b788cbe99a5941b1eac
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-10 11:32:09 -0700

    Update ✍🏼 2_SQL_DataCleansingOverView.sql

--- ✍🏼 2_SQL_DataCleansingOverView.sql
+++ ✍🏼 2_SQL_DataCleansingOverView.sql
@@ -10,6 +10,7 @@ STEPS
 SOURCE
 https://www.youtube.com/watch?v=8rO7ztF4NtU&list=PLUaB-1hjhk8H48Pj32z4GZgGWyylqv85f&index=3
 https://www.analystbuilder.com/courses/mysql-for-data-analytics
+https://www.youtube.com/watch?v=X6-VNKe3XVM
 */
 
 /* =================================================================================================================================
