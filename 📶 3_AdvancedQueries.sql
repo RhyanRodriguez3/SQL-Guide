82a5888e48f920e5ad9805501b9b6e7381deba24
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-10 22:58:00 -0700

    Update 📶 3_AdvancedQueries.sql

--- 📶 3_AdvancedQueries.sql
+++ 📶 3_AdvancedQueries.sql
@@ -3,13 +3,7 @@ This is a SQL guide for advanced level queries.
 We will be going over CTEs and temptables, recursive/hierachical queries, merge queries, and pivot queries. 
 */
 
--- How to handle duplicate rows.
-WITH TableCTE AS
-    (
-    SELECT*,  ROW_NUMBER() OVER(PARTITION BY ID ORDER BY ID) AS RowNumber
-    FROM Employees
-    )
-DELETE FROM TableCTE WHERE RowNumber > 1 
+
 
     
 -- Common Table Expressions are 
