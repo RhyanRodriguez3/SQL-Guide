0e1ccb69155bf33dae70630174c52ab0a5f960d2
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-04-10 23:24:48 -0700

    Update 📶 3_AdvancedQueries.sql

--- 📶 3_AdvancedQueries.sql
+++ 📶 3_AdvancedQueries.sql
@@ -3,7 +3,17 @@ This is a SQL guide for advanced level queries.
 We will be going over CTEs and temptables, recursive/hierachical queries, merge queries, and pivot queries. 
 */
 
-
+-- TRIGGERS: A block of code that defines a certain action when a certain operation is performed on the db.
+CREATE TABLE TRIGGER_TEST (MESSAGE VARCHAR(100))
+-- Apparently you have to use the CLI to create the trigger code.
+DELIMITER $$    -- Normally, the SSMS delimiter is a semicolon, so $$ is used to defined the delimiter
+CREATE
+    TRIGGER YourTriggerName BEFORE INSERT
+    ON table1
+    FOR EACH ROW BEGIN
+        INSERT INTO TRIGGER_TEST VALUES('added new employee');
+    END$
+DELIMITER;
 
     
 -- Common Table Expressions are 
@@ -15,15 +25,9 @@ We will be going over CTEs and temptables, recursive/hierachical queries, merge 
         INNER JOIN tbl1.IDColumnName = tbl2.IDColumnName
     )
 
--- Merge queries are ...
-    MERGE INTO target_table USING source_table 
-    ON merge_condition
-    WHEN MATCHED THEN 
-        UPDATE SET column1 = value1, column2 = value2,...
-    WHEN NOT MATCHED THEN 
-        INSERT (column1, column2,...) VALUES (value1, value2,...)
-    WHEN NOT MATCHED BY SOURCE THEN
-        DELETE;
+
+
+        
 
 https://www.youtube.com/watch?v=UC7uvOqcUTs&list=PLUaB-1hjhk8GjfgvWlreA6BvTvazz8RHG
 https://www.youtube.com/watch?v=C7CPXeEvKN0
