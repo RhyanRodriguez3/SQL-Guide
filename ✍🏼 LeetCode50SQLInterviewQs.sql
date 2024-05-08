aa78fbf1c2983c5c520508e74ba95f2f340b618e
Rhyan Rodriguez <70857572+RhyanRodriguez3@users.noreply.github.com>
2024-05-08 13:57:12 -0700

    Create ✍🏼 LeetCode50SQLInterviewQs.sql

---
+++ ✍🏼 LeetCode50SQLInterviewQs.sql
@@ -0,0 +1,7 @@
+/*===============================================================
+🟢https://leetcode.com/studyplan/top-sql-50/
+🔴SOURCES: https://www.youtube.com/@EverydayDataScience/playlists
+=================================================================*/
+
+/* Question 2: Using OR for multiple conditions */
+
